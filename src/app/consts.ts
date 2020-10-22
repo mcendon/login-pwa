8dfa6c7c75163be32c3beb085736df229384e52a
Mauro Cendon Hidalgo <mau.cendon@gmail.com>
2020-10-22 13:30:31 +0200

    add sign in error consts and login error management

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -1,3 +1,6 @@
 export const LAST_SIGN_IN_TIME_KEY = 'lastSignInTime';
 export const FIRESTORE_ERROR = 1;
 export const SIGN_IN_ERROR = 2;
+export const FIREAUTH_USER_DISABLED = 'auth/user-disabled';
+export const FIREAUTH_WRONG_PASSWORD = 'auth/wrong-password';
+export const FIREAUTH_NOT_FOUND = 'auth/user-not-found';
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -3,6 +3,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
+import {
+  SIGN_IN_ERROR,
+  FIRESTORE_ERROR,
+  FIREAUTH_NOT_FOUND,
+  FIREAUTH_USER_DISABLED,
+  FIREAUTH_WRONG_PASSWORD
+} from '../../consts';
 
 @Component({
   selector: 'app-login',
@@ -72,16 +79,35 @@ export class LoginComponent implements OnInit {
     Swal.showLoading();
     this.auth
       .login(form.get('email').value, form.get('password').value)
-      .then((data) => {
+      .then(() => {
         Swal.close();
         this.router.navigate(['welcome']);
       })
-      .catch(() => {
-        Swal.fire({
-          title: 'Incorrect Login',
-          icon: 'error',
-          text: 'Your email or password is wrong.',
-        });
+      .catch((e) => {
+        if (e.type === SIGN_IN_ERROR) {
+          const {code, message} = e.error;
+          if (code === FIREAUTH_USER_DISABLED) {
+            Swal.fire({
+              title: 'User disabled',
+              icon: 'info',
+              text: message,
+            });
+          } else if (code === FIREAUTH_WRONG_PASSWORD || code === FIREAUTH_NOT_FOUND) {
+            Swal.fire({
+              title: 'Incorrect Login',
+              icon: 'error',
+              text: 'Your email or password is wrong.',
+            });
+          } else {
+            Swal.close();
+          }
+        } else if (e.type === FIRESTORE_ERROR) {
+          Swal.fire({
+            title: 'Server problem',
+            icon: 'warning',
+            text: 'Our servers are experiencing problems. Please, try again later.',
+          });
+        }
         this.resetForm();
       });
   }
