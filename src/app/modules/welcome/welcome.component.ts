db2a0cbfccdbc1e042f42c9d1e553d5998a44c29
Mauro Cendón Hidalgo <mauro.cendon@gft.com>
2021-02-09 14:04:11 +0100

    Add display name

--- src/app/modules/welcome/welcome.component.ts
+++ src/app/modules/welcome/welcome.component.ts
@@ -8,8 +8,18 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class WelcomeComponent implements OnInit {
   time: number;
+  userDisplayName: string;
   constructor(private auth: AuthService, private router: Router) {
-    const lastSignInTime = auth.getLastSignInTime();
+    this.setWelcomePageData();
+  }
+
+  setWelcomePageData = () => {
+    const lastSignInTime = this.auth.getLastSignInTime();
+    this.auth.getUser().subscribe(user => {
+      if (user) {
+        this.userDisplayName = user.displayName || 'Anonymous';
+      }
+    });
     if (lastSignInTime) {
       this.time = new Date().getTime() - lastSignInTime;
     }
